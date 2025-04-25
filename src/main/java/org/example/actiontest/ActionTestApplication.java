4f059e129c5d3afb4f642bdeb785ac3d280162d0
SEUNGREEJEON <jws08219@naver.com>
2025-04-25 09:38:07 +0900

    init

---
+++ src/main/java/org/example/actiontest/ActionTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.actiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/actiontest/ActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.actiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
